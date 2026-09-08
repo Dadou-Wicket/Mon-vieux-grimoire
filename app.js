4cce0e54a6adb6bc49e9de26073ce7bab36c15af
Dadou Wicket <sbar54@hotmail.fr>
2026-09-08 11:30:53 +0200

    feat: structure book routes and controllers

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 
 const express = require("express");
+const bookRoutes = require("./routes/book");
 const mongoose = require("mongoose");
 const app = express();
+app.use("/api/books", bookRoutes);
 const Book = require("./models/book");
 const User = require("./models/user");
 
@@ -10,3 +12,7 @@ mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch((error) => console.log("Erreur MongoDB :", error.message));
+
+app.listen(3000, () => {
+  console.log("Serveur démarré sur le port 3000");
+});
---
+++ controllers/book.js
@@ -0,0 +1,11 @@
+exports.getAllBooks = (req, res, next) => {};
+
+exports.getOneBook = (req, res, next) => {};
+
+exports.createBook = (req, res, next) => {};
+
+exports.updateBook = (req, res, next) => {};
+
+exports.deleteBook = (req, res, next) => {};
+
+exports.rateBook = (req, res, next) => {};
