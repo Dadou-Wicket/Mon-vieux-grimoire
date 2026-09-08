c8a4d7c02b0cb882e8157c27d7ba59ba1a366df2
Dadou Wicket <sbar54@hotmail.fr>
2026-09-08 11:00:50 +0200

    feat: connect application to MongoDB

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
+require("dotenv").config();
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
 
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(3000, () => {
-  console.log("Serveur démarré sur le port 3000");
-});
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch((error) => console.log("Erreur MongoDB :", error.message));
