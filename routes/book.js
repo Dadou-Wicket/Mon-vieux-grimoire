9ea008c0510837f32573cc2cdc76f47bb4ae3152
Dadou Wicket <sbar54@hotmail.fr>
2026-09-08 12:47:15 +0200

    feat: add authentication middleware

--- routes/book.js
+++ routes/book.js
@@ -1,9 +1,21 @@
 const express = require("express");
+
 const bookCtrl = require("../controllers/book");
+
+const auth = require("../middleware/auth");
 
 const router = express.Router();
 
 router.get("/", bookCtrl.getAllBooks);
+
 router.get("/:id", bookCtrl.getOneBook);
+
+router.post("/", auth, bookCtrl.createBook);
+
+router.put("/:id", auth, bookCtrl.updateBook);
+
+router.delete("/:id", auth, bookCtrl.deleteBook);
+
+router.post("/:id/rating", auth, bookCtrl.rateBook);
 
 module.exports = router;
