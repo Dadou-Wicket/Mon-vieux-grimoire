d5b639dea4ec73066a1135ded31ce491da0f66df
Dadou Wicket <sbar54@hotmail.fr>
2026-09-08 11:11:05 +0200

    feat: add book data model

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const Book = require("./models/book");
 
 mongoose
   .connect(process.env.MONGODB_URI)
