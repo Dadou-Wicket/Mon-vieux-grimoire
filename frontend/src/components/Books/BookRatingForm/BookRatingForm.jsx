8a02e3aa15d4dfceab291ab90bb03dfc21852ac9
Dadou Wicket <sbar54@hotmail.fr>
2026-09-08 16:25:44 +0200

    test: validate book management features

--- frontend/src/components/Books/BookRatingForm/BookRatingForm.jsx
+++ frontend/src/components/Books/BookRatingForm/BookRatingForm.jsx
@@ -42,9 +42,11 @@ function BookRatingForm({
   return (
     <div className={styles.BookRatingForm}>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <p>{rating > 0 ? 'Votre Note' : 'Notez cet ouvrage'}</p>
+        <p>{userRated ? 'Merci !' : 'Notez cet ouvrage'}</p>
         <div className={styles.Stars}>
-          {!userRated ? generateStarsInputs(rating, register) : displayStars(rating)}
+          {!userRated
+            ? generateStarsInputs(rating, register)
+            : displayStars(rating)}
         </div>
         {!userRated ? <button type="submit">Valider</button> : null}
       </form>
