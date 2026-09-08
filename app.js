d926747ce8027618d2758e7c1057b894d4f6d996
Dadou Wicket <sbar54@hotmail.fr>
2026-09-08 11:14:49 +0200

    feat: add user data model

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const Book = require("./models/book");
+const User = require("./models/user");
 
 mongoose
   .connect(process.env.MONGODB_URI)
